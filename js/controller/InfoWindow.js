1baea2a6461ebca291987c2e12fdc2d721534407
Yu Kusakabe <Yu@Yus-MacBook-Pro.local>
2014-10-14 15:10:13 -0700

    make AgencyList

---
+++ js/controller/InfoWindow.js
@@ -0,0 +1,12 @@
+/*
+*CircleOverlay Class
+*/
+delerrok.mapp.InfoWindow = function(map, agencyData) {
+	this.gmap = map;
+	this.agencyData = agencyData;
+	
+	this.infoWindows = null;
+}
+
+delerrok.mapp.InfoWindow.prototype.init = function() {
+};
