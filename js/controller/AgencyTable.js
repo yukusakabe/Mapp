4b0bd470c966d9ecff901b71174f6f0310649f9a
Yu Kusakabe <Yu@Yus-MacBook-Pro.local>
2014-10-14 15:10:23 -0700

    make AgencyList

---
+++ js/controller/AgencyTable.js
@@ -0,0 +1,133 @@
+/*
+*AgencyTable Class
+*/
+delerrok.mapp.AgencyTable = function(agencyData, tbody) {
+	this.agencyData = agencyData;
+	this.tableData = [];
+
+	this.thead = null;
+	this.tbody = tbody;
+	
+	this.nowSortKey = null;
+	this.nowOrder = true;
+	this.nowSelect = null;
+}
+
+delerrok.mapp.AgencyTable.prototype.init = function(mapType, transitType) {
+	var agencyList = this.agencyData.getAgencyList();
+
+	for (var k in agencyList) {
+		this.tableData.push([this.agencyData.getId(k), this.agencyData.getName(k), this.agencyData.getAbbr(k), this.agencyData.getCity(k), this.agencyData.getState(k), this.agencyData.getCountry(k), this.agencyData.getRidership(k, transitType)]);
+	}
+};
+
+delerrok.mapp.AgencyTable.prototype.sortTable = function(sortKey, order) {
+	this.nowSortKey = sortKey;
+
+	switch (sortKey) {
+		case SortKey.ID:
+		case SortKey.RIDERSHIP:
+			this.tableData.sort(function(a, b) {
+				if (order == true) {
+					return (a[sortKey] - b[sortKey]);
+				} else {
+					return (b[sortKey] - a[sortKey]);
+				}	
+			});
+			break;
+			
+		case SortKey.NAME:
+		case SortKey.ADDR:
+		case SortKey.CITY:
+		case SortKey.STATE:
+		case SortKey.COUNTRY:
+			this.tableData.sort(function(a, b) {
+				if (order == true) {
+					if(a[sortKey] > b[sortKey])  return 1;
+					if(a[sortKey] < b[sortKey])  return -1;
+				} else {
+					if(a[sortKey] > b[sortKey])  return -1;
+					if(a[sortKey] < b[sortKey])  return 1;
+				}	
+			});
+			break;
+	}
+};
+
+delerrok.mapp.AgencyTable.prototype.createTdElement = function(txt, type) {
+	var tdElm = document.createElement("td");
+	switch (type) {
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+			tdElm.setAttribute('class', 'tdBodyL');
+			break;
+		
+		case 0:
+		case 6:
+			tdElm.setAttribute('class', 'tdBodyR');
+			break;
+			
+
+			tdElm.setAttribute('class', 'tdBodyC');
+			break;
+	}
+	
+	var divElm = document.createElement("div");
+	divElm.setAttribute('class', 'tableElmDiv');
+	divElm.setAttribute('title', txt);
+	var txtObj = document.createTextNode(txt);
+	divElm.appendChild(txtObj);
+	tdElm.appendChild(divElm);
+	return tdElm;
+};
+
+delerrok.mapp.AgencyTable.prototype.createTrElement = function(data) {
+	var trElm = document.createElement('tr');
+	trElm.setAttribute('id', data[0]);
+	trElm.setAttribute('data-AgencyId', data[0]);
+	trElm.setAttribute('class', 'trBody');
+	trElm.setAttribute('onClick', 'delerrok.mapp.clickAgencyTable(this.id)');
+	trElm.appendChild(this.createTdElement(data[1], 1));
+	trElm.appendChild(this.createTdElement(data[2], 2));
+	trElm.appendChild(this.createTdElement(data[3], 3));
+	trElm.appendChild(this.createTdElement(data[4], 4));
+	trElm.appendChild(this.createTdElement(data[5], 5));
+	trElm.appendChild(this.createTdElement(data[6], 6));
+	return trElm;	
+};
+
+delerrok.mapp.AgencyTable.prototype.createTable  = function() {
+	for (var i = 0; i < this.tableData.length; i++) {
+		this.tbody.appendChild(this.createTrElement(this.tableData[i]));
+	}
+}
+
+delerrok.mapp.AgencyTable.prototype.makeTable = function(sortKey, order) {
+	this.nowSortKey = sortKey;
+	this.nowOrder = order;
+	this.sortTable(sortKey, order);
+
+	this.createTable();
+}
+
+delerrok.mapp.AgencyTable.prototype.deleteTable = function() {
+	for (var i = this.tbody.childNodes.length - 1; i >= 0; i--) {
+		this.tbody.removeChild(this.tbody.childNodes[i]);
+	}
+}
+
+delerrok.mapp.AgencyTable.prototype.changeTable = function(sortKey) {
+	if (sortKey == this.nowSortKey) {
+		this.nowOrder = !this.nowOrder;
+	} else {
+		this.nowSortKey = sortKey;
+		this.nowOrder = true;
+	}
+	
+	this.sortTable(Number(this.nowSortKey), this.nowOrder);
+	this.deleteTable();
+	this.createTable();
+}
---
+++ js/controller/CircleOverlay.js
@@ -0,0 +1,108 @@
+/*
+*CircleOverlay Class
+*/
+delerrok.mapp.CircleOverlay = function(map, agencyData) {
+	this.gmap = map;
+	this.agencyData = agencyData;
+	
+	this.baseRule = null;
+	this.baseOpts = null;
+	this.defOptsTable = {};
+	this.defOpts = {};
+	
+	this.circleSize = null;
+	this.currentTransitType = null;
+		
+	this.circle = {};
+	
+	this.init();
+}
+
+delerrok.mapp.CircleOverlay.prototype.init = function(){		
+	this.baseRule = delerrok.mapp.readBaseRule();
+	this.baseOpts = delerrok.mapp.readBaseOpts();
+	
+	this.initDefOptsTable();
+	this.initDefOpts();
+};
+
+delerrok.mapp.CircleOverlay.prototype.getCurrentRadius = function(id) {
+	return this.defOpts[id].getCRadius()
+}
+
+delerrok.mapp.CircleOverlay.prototype.initDefOptsTable = function() {
+	var agencyList = this.agencyData.getAgencyList();
+	
+	for (var k in agencyList) {
+		for (var i = 0; i < this.baseRule.length; i++) {
+			if(this.baseRule[i].applyRule(this.agencyData.getRidership(k, TransitType.TOTAL), this.agencyData.getAgencyRank(k))) {
+				this.defOptsTable[k] = this.baseRule[i].getOptsNo();
+			}
+		}
+	}
+};
+
+delerrok.mapp.CircleOverlay.prototype.initDefOpts = function() {
+	for (var k in this.defOptsTable) {
+		this.defOpts[k] = this.baseOpts[this.defOptsTable[k]].makeCopy();
+		this.defOpts[k].setCCenter(this.agencyData.getLatitude(k), this.agencyData.getLongitude(k));
+		this.defOpts[k].setCMap(this.gmap);
+		this.defOpts[k].setCZIndex(this.agencyData.getAgencyRank(k));
+	}
+};
+
+delerrok.mapp.CircleOverlay.prototype.drawGraph = function(size, transitType) {
+	this.circleSize = size;
+	this.currentTransitType = transitType;
+
+	for (var k in this.defOpts) {
+		this.defOpts[k].setCRadius(this.calcRadius(this.agencyData.getRidership(k, transitType), this.agencyData.getLatitude(k)));
+		this.circle[k] = new google.maps.Circle(this.defOpts[k]);
+	}
+};
+
+delerrok.mapp.CircleOverlay.prototype.getCircle = function(id) {
+	return this.circle[id];
+};
+
+delerrok.mapp.CircleOverlay.prototype.showCircle = function(id) {
+	this.circle[id].setVisible(true);
+};
+
+delerrok.mapp.CircleOverlay.prototype.showAllCircle = function() {
+	for (var k in this.circle) {
+		this.circle[k].setVisible(true);
+	}
+};
+
+delerrok.mapp.CircleOverlay.prototype.hideCircle = function(id) {
+	this.circle[id].setVisible(false);
+};
+
+delerrok.mapp.CircleOverlay.prototype.hideAllCircle = function(id) {
+	for (var k in this.circle) {
+		this.circle[k].setVisible(false);
+	}
+};
+
+delerrok.mapp.CircleOverlay.prototype.changeCircleSize = function(size) {
+	this.circleSize = size;
+	
+	for (var k in this.circle) {
+		this.defOpts[k].setCRadius(this.calcRadius(this.agencyData.getRidership(k, this.currentTransitType), this.agencyData.getLatitude(k)));
+		this.circle[k].setRadius(this.defOpts[k].getCRadius());
+	}
+};
+
+delerrok.mapp.CircleOverlay.prototype.changeTransitType = function(transitType) {
+	this.currentTransitType = transitType;
+
+	for (var k in this.circle) {
+		this.defOpts[k].setCRadius(this.calcRadius(this.agencyData.getRidership(k, this.currentTransitType), this.agencyData.getLatitude(k)));
+		this.circle[k].setRadius(this.defOpts[k].getCRadius());
+	}
+};
+
+delerrok.mapp.CircleOverlay.prototype.calcRadius = function(radius, latitude) {
+	return this.circleSize * Math.sqrt(radius) * Math.cos(latitude * (Math.PI / 180), 2);
+};
